3a41f4cb2923e638fe501ec4308b886610f1c6ca
xg4 <xingor4@gmail.com>
2021-05-25 21:02:00 +0800

    chore: mobile design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { useState } from 'react'
 import { toast } from '../components/Toast'
 
@@ -29,13 +30,17 @@ export default function Home() {
   }
 
   return (
-    <div className="mx-auto mt-48 mb-20 text-center">
+    <div className="container mx-auto mt-40 mb-20 text-center px-10">
       <h2 className="text-lg font-bold mb-10 text-gray-700">Nano URL</h2>
-      <div className="shadow inline-flex items-center">
+      <div
+        className={clsx('w-full inline-flex items-center justify-center', {
+          'flex-col sm:flex-row': !!redirect,
+        })}
+      >
         {redirect ? (
           <>
             <a
-              className="text-blue-500 px-2 hover:underline"
+              className="text-blue-500 px-2 hover:underline mb-10 sm:mb-0"
               href={redirect}
               target="_blank"
             >
@@ -55,7 +60,7 @@ export default function Home() {
             <input
               type="text"
               placeholder="Enter a URL to shorten..."
-              className="w-80 px-2 py-3 focus:outline-none focus:ring-1 focus:ring-indigo-300 focus:border-transparent"
+              className="max-w-sm flex-1 px-2 py-3 focus:outline-none focus:ring-1 focus:ring-indigo-300 focus:border-transparent"
               value={link}
               onChange={(evt) => {
                 setLink(evt.target.value)
