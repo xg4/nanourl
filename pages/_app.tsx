58314fb157376d94033280f579daa0c4a8c30cf5
xg4 <xingor4@gmail.com>
2021-05-25 18:29:53 +0800

    chore: update tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Nano URL</title>
+        <title>
+          URL Shortener - Short URLs & Custom Free Link Shortener | Nano URL
+        </title>
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Free URL shortener to create perfect URLs for your business. It's open source"
+        />
       </Head>
       <Component {...pageProps} />
     </>
