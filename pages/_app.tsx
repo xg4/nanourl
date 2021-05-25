39e6e221bc958360c1ffcb1e8930505d36abac57
xg4 <xingor4@gmail.com>
2021-05-25 18:09:14 +0800

    chore: add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/tailwind.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Nano URL</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
