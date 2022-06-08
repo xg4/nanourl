bd78934fb2b661967ccb5cf2bc5ae9ac4524f19c
xg4 <xingor4@gmail.com>
2022-06-08 15:59:31 +0800

    chore: revert shortCode func

--- util/shared.ts
+++ util/shared.ts
@@ -5,14 +5,9 @@ export function isURL(url: string) {
 }
 
 export function getShortCode(id: number): string {
-  const allCount = 62 ** 6
-  return base62
-    .encode(id >= allCount ? allCount - id : allCount / 2 - id)
-    .padStart(6, '0')
+  return base62.encode(id).padStart(6, '0')
 }
 
 export function getIdByShortCode(base62Str: string): number {
-  const allCount = 62 ** 6
-  const id = base62.decode(base62Str)
-  return id >= allCount ? allCount - id : allCount / 2 - id
+  return base62.decode(base62Str)
 }
