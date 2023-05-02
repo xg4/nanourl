77e8f35a95ed3e0a93e25b03f0083ad6905eaf8b
xg4 <xingor4@gmail.com>
2023-05-02 12:11:35 +0800

    chore: update db

--- prisma/migrations/20230502041116_init/migration.sql
+++ prisma/migrations/20230502041116_init/migration.sql
@@ -1,19 +1,19 @@
 -- CreateTable
 CREATE TABLE "links" (
     "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "shortCode" TEXT NOT NULL,
+    "short_code" TEXT NOT NULL,
     "original_url" TEXT NOT NULL,
     "hash" TEXT NOT NULL,
     "expires_at" TIMESTAMP(3),
     "clicks" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "links_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "links_shortCode_key" ON "links"("shortCode");
+CREATE UNIQUE INDEX "links_short_code_key" ON "links"("short_code");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "links_original_url_key" ON "links"("original_url");
