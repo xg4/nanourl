eba6c69329f2b2f5d5c691caf43bc8c94caca79b
xg4 <xingor4@gmail.com>
2023-05-04 17:27:41 +0800

    feat: add vercel db, analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import { ConfigProvider } from 'antd'
 import 'antd/dist/reset.css'
 import type { AppProps } from 'next/app'
@@ -10,21 +11,24 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <ConfigProvider>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Head>
-            <title>URL Shortener - Short URLs & Custom Free Link Shortener | Nano URL</title>
-            <link rel="icon" href="/favicon.ico" />
-            <meta charSet="utf-8" />
-            <meta
-              name="description"
-              content="Free URL shortener to create perfect URLs for your business. It's open source"
-            />
-          </Head>
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
-    </ConfigProvider>
+    <>
+      <ConfigProvider>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Head>
+              <title>URL Shortener - Short URLs & Custom Free Link Shortener | Nano URL</title>
+              <link rel="icon" href="/favicon.ico" />
+              <meta charSet="utf-8" />
+              <meta
+                name="description"
+                content="Free URL shortener to create perfect URLs for your business. It's open source"
+              />
+            </Head>
+            <Component {...pageProps} />
+          </Hydrate>
+        </QueryClientProvider>
+      </ConfigProvider>
+      <Analytics />
+    </>
   )
 }
